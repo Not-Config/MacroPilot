a0cd7f74399047dd8f92012bec4c12c189bacb66
Not.Config <83444054+Not-Config@users.noreply.github.com>
2026-08-07 14:00:12 +0300

    Add official project support link

--- project_config.py
+++ project_config.py
@@ -4,8 +4,7 @@ AUTHOR_NAME = "Config"
 AUTHOR_GITHUB = "Not-Config"
 PROJECT_REPOSITORY = f"{AUTHOR_GITHUB}/MacroPilot"
 PROJECT_URL = f"https://github.com/{PROJECT_REPOSITORY}"
-SUPPORT_URL = f"{PROJECT_URL}/blob/main/SUPPORT.md"
+SUPPORT_URL = "https://www.tbank.ru/cf/5bn6k43J3oJ"
 
 WINDOWS_RELEASE_ASSET = "MacroPilot-windows.zip"
 SOURCE_RELEASE_ASSET = "MacroPilot-source.zip"
-
